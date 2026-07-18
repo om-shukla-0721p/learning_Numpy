7b45f40d01d67d4c5fbf4c04421de13fcba9437d
omshukla0721p <ks0134067@gmail.com>
2026-07-18 19:10:55 +0530

    new files

--- numpy_setup.py
+++ numpy_setup.py
@@ -1,13 +1,20 @@
 import numpy as np
 
-arr=np.array([[["a","b","c"],
+array=np.array([[["a","b","c"],
               ["a","b","c"],
               ["a","b","c"]] for i in range(3)])
 
-print(arr.ndim)
+print(array.ndim)
 
-word=arr[0,0,0]+arr[0,1,1]+arr[0,2,2]
+word=array[0,0,0]+array[0,1,1]+array[0,2,2]
 print(word)
 # what
 
+
+arr=        [[1, 2 ,3 ,4],
+             [5, 6 ,7 ,8],
+             [9, 10,11,12],
+             [13,14,15,16]]
+
+print(arr[row[0:2]] for row in range(0,2))
 
